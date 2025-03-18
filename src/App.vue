d92a8be96cd8d6c3a9a2d29c33be326e965d7ea8
carolmeireles <carolinemeireles.si@gmail.com>
2025-03-18 16:47:02 -0300

    operação funcionando

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,42 @@
 <script setup>
 
+import { reactive } from 'vue';
+import Operacao from "./components/Operacao.vue";
+
+const estado = reactive({
+    filtro: 'soma',
+    num1: 0,
+    num2: 0
+});
+
+function realizaOperacao() {
+    const { filtro, num1, num2 } = estado;
+    switch (filtro) {
+      case 'soma':
+        return num1 + num2;
+      case 'subtracao':
+        return num1 - num2;
+      case 'multiplicacao':
+        return num1 * num2;
+      case 'divisao':
+        return num1 / num2;
+    }
+}
+
 </script>
 
 <template>
+
+<header>
+    <h1>Calculadora Aritmética</h1>
+</header>
+
+<Operacao
+  :filtrar-operacao="(event) => (estado.filtro = event.target.value)"
+  :numero1="(event) => (estado.num1 = Number(event.target.value))"
+  :numero2="(event) => (estado.num2 = Number(event.target.value))" />
+
+  = {{ realizaOperacao() }}
 
 </template>
 
---
+++ src/components/Operacao.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+const props = defineProps(["filtrarOperacao", "numero1", "numero2"]);
+
+</script>
+
+<template>
+
+<form>
+    <input type="number" placeholder="Digite um número" @keyup="props.numero1" />
+    <select @change="props.filtrarOperacao">
+        <option value="soma">+</option>
+        <option value="subtracao">-</option>
+        <option value="multiplicacao">*</option>
+        <option value="divisao">/</option>
+    </select>
+    <input type="number" placeholder="Digite um número" @keyup="props.numero2" />
+</form>
+
+</template>
+
+<style scoped>
+
+</style>
